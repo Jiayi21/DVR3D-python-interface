459cbff1d90da9475314d8aec9061f713d89dd38
Jiayi He <2827266204@qq.com>
2022-06-08 18:11:34 +0100

    parse: update help message

--- DVR3Dinterface/parse.py
+++ DVR3Dinterface/parse.py
@@ -5,8 +5,8 @@ from dict.dvr3dparser import txtToJson,GeneralParser
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Generate job file from input file")
-    parser.add_argument("input",help="Path to the input file")
-    parser.add_argument("config",help="What config to use (no need to add .json)")
+    parser.add_argument("input",help="Path to the input file, for example: input/example_rot.txt")
+    parser.add_argument("config",help="What config to use, input same name as the json file in configs, no need to add .json")
     parser.add_argument("-o","--output",help="Name of output file, config name + .job by default")
     
     args = parser.parse_args()
