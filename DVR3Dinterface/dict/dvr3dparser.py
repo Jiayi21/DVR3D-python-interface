cbcad14a972c10e19ad83cd04decee627173829c
Jiayi He <2827266204@qq.com>
2022-06-08 17:59:50 +0100

    gitignore, commandline parse input to job

---
+++ DVR3Dinterface/dict/dvr3dparser.py
@@ -0,0 +1,108 @@
+from fortranformat import FortranRecordWriter as ffW
+from pathlib import Path
+import json
+
+class GeneralParser:
+    config = {}
+
+    def __init__(self,config):
+        with open (Path(config)) as f:
+            self.config=json.load(f)
+
+    def __PrtToStr(self,name,bol):
+        if bol:
+            name += "=.true., "
+        else:
+            name += "=.false., "
+        return name
+    
+    # Parse and print the NAMELIST line
+    def __parPRT(self,configsub,data,filestream):
+        filestream.write(" {} ".format(configsub["head"]))
+        # Check keylist_A, all optional
+        for var in configsub["keylist_A"]:
+            if var in data:
+                filestream.write(var.lower()+"="+data[var]+" ")
+        
+        # Check keylist_L, all optional
+        for var in configsub["keylist_L"]:
+            if var in data:
+                filestream.write(self.__PrtToStr(var.lower(),data[var]))
+        
+        # Change Line, and write the "/" at the end of first line.
+        filestream.write(" / \n")
+
+    # Parse the variable line
+    # All variable should have same type
+    def __parVAL(self,configsub,data,filestream):
+        countval = 0
+        writer = ffW(configsub["format"])
+        for var in configsub["keylist"]:
+            if var in data:
+                filestream.write(writer.write([data[var]]))
+                countval+=1
+            elif countval<configsub["minval"]:
+                raise KeyError("Mandatory variable not provided: {}".format(var))
+            else:
+                break
+
+        filestream.write("\n")
+
+    # Print title
+    def __parTITLE(self,configsub,data,filestream):
+        if len(data["TITLE"]) > configsub["length"]:
+            print("Warning: Title longer than config set length, will be capped")
+        filestream.write(data["TITLE"][:configsub["length"]]+"\n")
+
+    # Customised format
+    def __parCUS(self,configsub,data,filestream):
+        countval = 0
+        for var in configsub["keylist"]:
+            if var in data:
+                writer = ffW(configsub["keylist"][var])
+                filestream.write(writer.write([data[var]]))
+                countval+=1
+            elif countval<configsub["minval"]:
+                raise KeyError("Mandatory variable not provided: {}".format(var))
+            else:
+                break
+
+        filestream.write("\n")
+
+
+    def write(self,input,output):
+        with open (Path(output),"w+",encoding="utf-8") as f:
+            for line in self.config:
+                try:
+                    if self.config[line]["type"] == "PRT":
+                        self.__parPRT(self.config[line],input,f)
+                    elif self.config[line]["type"] == "VAL":
+                        self.__parVAL(self.config[line],input,f)
+                    elif self.config[line]["type"] == "TITLE":
+                        self.__parTITLE(self.config[line],input,f)
+                    elif self.config[line]["type"] == "CUS":
+                        self.__parCUS(self.config[line],input,f)
+                    else:
+                        raise ValueError("Config type not found: {}".format(line))
+                except Exception as e:
+                    print("Error parsing {}: {}".format(line,e))
+                    raise
+
+def txtToJson(filepath):
+    pathin = Path(filepath)
+    pathout = Path(filepath+".json")
+    with open(pathin,"r",encoding="utf-8") as fin:
+        with open(pathout,"w+",encoding="utf-8",newline="") as fout:
+            fout.write("{\n")
+            line = fin.readline()
+            while(line != ""):
+                try:   
+                    lineVec = line.split(":")
+                    fout.write("\"{}\":{}".format(lineVec[0],lineVec[1]))
+                    line = fin.readline()
+                    if (line != ""): fout.write(",")
+                except Exception as e:
+                    print("Failed reading input, check file format: {}".format(line))
+                    raise
+            fout.write("}")
+    return pathout
---
+++ DVR3Dinterface/parse.py
@@ -0,0 +1,25 @@
+import argparse
+import json
+from pathlib import Path
+from dict.dvr3dparser import txtToJson,GeneralParser
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Generate job file from input file")
+    parser.add_argument("input",help="Path to the input file")
+    parser.add_argument("config",help="What config to use (no need to add .json)")
+    parser.add_argument("-o","--output",help="Name of output file, config name + .job by default")
+    
+    args = parser.parse_args()
+
+    jsonfile = txtToJson(args.input)
+    dvrparser = GeneralParser(Path("configs/"+args.config + ".json"))
+
+    outfilename = args.config+".job"
+    if (args.output):
+        outfilename = args.output
+    
+    with open (jsonfile) as f:
+        jsonin = json.load(f)
+    
+    dvrparser.write(jsonin,Path("output/"+outfilename))
+
