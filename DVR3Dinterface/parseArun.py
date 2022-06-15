d088c681476b2ffe0bff560a2c6857bbad34bd6a
Jiayi He <2827266204@qq.com>
2022-06-15 14:26:20 +0100

    Add option to parseArun: run make or not

--- DVR3Dinterface/parseArun.py
+++ DVR3Dinterface/parseArun.py
@@ -8,9 +8,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Test running Fortran from python")
     parser.add_argument("input",help="Path to the input file, for example: input/example_rot.txt")
     parser.add_argument("config",help="What config to use, input same name as the json file in configs, no need to add .json")
-    parser.add_argument("Fsource",help="Fortran code to compile and run")
+    parser.add_argument("Fsource",help="Fortran code to run")
     parser.add_argument("-o","--output",help="Name of output file, config name + .result by default")
     parser.add_argument("-j","--job",action="store_true",help="If set, the temp.job file will not be removed")
+    parser.add_argument("-m","--make",action="store_true",help="If set, run make Fsource before executing Fortran code")
 
     args = parser.parse_args()
 
@@ -18,24 +19,30 @@ if __name__ == '__main__':
     dvrparser = GeneralParser(Path("configs/"+args.config + ".json"))
 
 
-    
+    # Convert input file to JSON to read
     with open (jsonfile) as f:
         jsonin = json.load(f)
     
     os.remove(jsonfile)
     
+    # Parse to the job file
     dvrparser.write(jsonin,Path("output/temp.job"))
 
-
+    # Decide the output filename
     outfilename = args.config+".result"
     if (args.output):
         outfilename = args.output
 
-    os.system("make "+args.Fsource)
+    # Run make xxx or not
+    if (args.make):
+        os.system("make "+args.Fsource)
+
+    # Run Fortran
     os.system("./{} <output/temp.job> {}".format(args.Fsource, outfilename))
 
+    # Remove intermediate job file or not
     if not args.job:
         os.remove("output/temp.job")
 
-    print("Successfully executed")
+    print("Executing finished")
 
