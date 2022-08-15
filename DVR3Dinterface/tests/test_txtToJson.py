cefe7bb33140dd03f44c301f58de05115474e377
Jiayi He <2827266204@qq.com>
2022-08-15 11:16:06 +0100

    Fix tests

--- DVR3Dinterface/tests/test_txtToJson.py
+++ DVR3Dinterface/tests/test_txtToJson.py
@@ -8,11 +8,16 @@ from pytest import raises
 testpath = "DVR3Dinterface/tests/txtToJsonData/"
 
 def test_positive():
+    filecmp.clear_cache()
     dvrparser.txtToJson(testpath+"positive.txt")
+    # Note: Sometimes this compare return False even if files are same
+    # If the assertion fails, please check manually
     assert filecmp.cmp(testpath+"positiveSample.json",
-                        testpath+"positive.txt.json")
+                        testpath+"positive.txt.json",shallow=True)
 
 def test_positive_comment():
     dvrparser.txtToJson(testpath+"p_comment.txt")
+    # Note: Sometimes this compare return False even if files are same
+    # If the assertion fails, please check manually
     assert filecmp.cmp(testpath+"positiveSample.json",
-                        testpath+"p_comment.txt.json")
+                        testpath+"p_comment.txt.json",shallow=True)
